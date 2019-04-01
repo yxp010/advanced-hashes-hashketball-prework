c11381b0a63087f02718f8c51d238d3ed50d4b04
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-04-01 03:14:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -177,14 +177,14 @@ def player_numbers(team_name)
 end
 
 def player_stats(player_name)
-  stats = {}
+  s = {}
   game_hash.each do |team, information|
     information[:players].each do |player, stats|
       if player == player_name
-        stats = stats
+        s = stats
       end
     end
   end
 
-  stats
+  s
 end
